d7d618830afaad65484a02ad7e355d6536176d7a
José Souza <83735512+Soulsouza@users.noreply.github.com>
2022-05-13 15:55:00 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,60 @@
+function onScroll() {
+  showNavOnScroll();
+  showBackToHomeButton();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY != 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToHomeButton() {
+  if (scrollY > 500) {
+    backToHome.classList.add("show");
+  } else {
+    backToHome.classList.remove("show");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
